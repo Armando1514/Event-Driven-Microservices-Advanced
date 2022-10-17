cc767043d9c2e83b091b6b9fc588687edaecfc8f
armando.ferrara <armando.ferrara@dormakaba.com>
2022-10-17 14:38:47 +0200

    kafka-modules added

---
+++ infrastructure/kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/exception/KafkaProducerException.java
@@ -0,0 +1,7 @@
+package com.food.ordering.system.kafka.producer.exception;
+
+public class KafkaProducerException extends RuntimeException{
+    public KafkaProducerException(String message) {
+        super(message);
+    }
+}
